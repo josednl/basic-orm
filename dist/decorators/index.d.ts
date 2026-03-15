52033971c55ebc0c50beb618db7d8ab794ef8235
JoseD <157433709+josednl@users.noreply.github.com.>
2026-03-14 21:26:13 -0500

    chore: remove node_modules and dist from git tracking

--- dist/decorators/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./entity";
-//# sourceMappingURL=index.d.ts.map
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Entity, Column, PrimaryColumn, PrimaryGeneratedColumn } from "./decorators";
-export { connection, Connection, ConnectionOptions } from "./core/connection/connection";
-export { Repository, FindOptions } from "./repositories/repository";
-export { metadataStorage, MetadataStorage } from "./core/metadata/metadata-storage";
-//# sourceMappingURL=index.d.ts.map
